231819fcaf4eab4f30fd9734c740f353f25d8b5c
Eric Hosford <eric.hosford@gmail.com>
2017-06-14 20:10:05 -0400

    created basic server and skeleton for app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express  = require('express');
+const server = express();
+const port = 8080;
+
+server.use(express.static(__dirname + 'public'));
+
+server.get('/', (req, res) => {
+  res.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(port, () => {
+  console.log('Server started on', port);
+});
