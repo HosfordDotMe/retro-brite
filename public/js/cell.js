481f252776e0765e4427b1dfccdef8d8469e1e78
Eric Hosford <eric.hosford@gmail.com>
2017-06-15 20:33:48 -0400

    refactored the makegrid method. Also implemented the render method to draw the grid on the page that required the cell to produce a dom element.

--- public/js/cell.js
+++ public/js/cell.js
@@ -12,6 +12,18 @@ function Cell(width, height, color) {
   this.color = color || 'red';
 }
 
+Cell.prototype.toHTML = function(){
+  const div = document.createElement('div');
+  div.style.height = `${this.height}px`;
+  div.style.margin = `0`;
+  div.style.display = 'inline-block';
+  div.style.width = `${this.width}px`;
+  div.style.border = `1px solid black`;
+  div.style.backgroundColor = this.color;
+  return div;
+};
+
+
 /**
  * Create a cell through a factory function
  * @param  {integer} width  width of Cell
